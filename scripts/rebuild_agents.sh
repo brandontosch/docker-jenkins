38c5ff3fd9e122425d7c3ddb6ad7b215c2d1041a
Brandon Tosch <Brandon.Tosch@veritas.com>
2017-10-09 16:19:02 -0700

    renamed slave to agent

--- scripts/rebuild_agents.sh
+++ scripts/rebuild_agents.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-docker rmi jenkins-slave-terraform
-docker rmi jenkins-slave-azurecli-vault
-docker rmi jenkins-slave-azurecli-packer
-docker rmi jenkins-slave-azurecli
-docker rmi jenkins-slave-git
-docker rmi jenkins-slave-alpine
+docker rmi jenkins-agent-terraform
+docker rmi jenkins-agent-azurecli-vault
+docker rmi jenkins-agent-azurecli-packer
+docker rmi jenkins-agent-azurecli
+docker rmi jenkins-agent-git
+docker rmi jenkins-agent-alpine
 
-docker build -t jenkins-slave-alpine ../agents/alpine/
-docker build -t jenkins-slave-git ../agents/git/
-docker build -t jenkins-slave-azurecli ../agents/azurecli/
-docker build -t jenkins-slave-azurecli-packer ../agents/azurecli-packer/
-docker build -t jenkins-slave-azurecli-vault ../agents/azurecli-vault/
-docker build -t jenkins-slave-terraform ../agents/terraform/
+docker build -t jenkins-agent-alpine ../agents/alpine/
+docker build -t jenkins-agent-git ../agents/git/
+docker build -t jenkins-agent-azurecli ../agents/azurecli/
+docker build -t jenkins-agent-azurecli-packer ../agents/azurecli-packer/
+docker build -t jenkins-agent-azurecli-vault ../agents/azurecli-vault/
+docker build -t jenkins-agent-terraform ../agents/terraform/
 
 docker images
