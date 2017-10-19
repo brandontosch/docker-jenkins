812e3efc75b0cbf0d6daf39e25cb8611a4667735
Brandon Tosch <brandon.tosch@veritas.com>
2017-10-19 15:11:40 -0700

    remove extra tools dir

--- agents/azurecli-packer/tools/download.sh
+++ agents/azurecli-packer/tools/download.sh
@@ -3,8 +3,8 @@ PACKER_VERSION=$1
 BASE_DIR=$(dirname "$0")
 
 curl -O $(printf "https://releases.hashicorp.com/packer/%s/packer_%s_linux_amd64.zip" $PACKER_VERSION $PACKER_VERSION)
-rm -f $BASE_DIR/tools/packer
-unzip $(printf "packer_%s_linux_amd64.zip" $PACKER_VERSION) -d $BASE_DIR/tools/
+rm -f $BASE_DIR/packer
+unzip $(printf "packer_%s_linux_amd64.zip" $PACKER_VERSION) -d $BASE_DIR/
 rm -f $(printf "packer_%s_linux_amd64.zip" $PACKER_VERSION)
 
-$BASE_DIR/tools/packer -v
+$BASE_DIR/packer -v
--- agents/terraform/tools/download.sh
+++ agents/terraform/tools/download.sh
@@ -3,8 +3,8 @@ TF_VERSION=$1
 BASE_DIR=$(dirname "$0")
 
 curl -O $(printf "https://releases.hashicorp.com/terraform/%s/terraform_%s_linux_amd64.zip" $TF_VERSION $TF_VERSION)
-rm -f $BASE_DIR/tools/terraform
-unzip $(printf "terraform_%s_linux_amd64.zip" $TF_VERSION) -d $BASE_DIR/tools/
+rm -f $BASE_DIR/terraform
+unzip $(printf "terraform_%s_linux_amd64.zip" $TF_VERSION) -d $BASE_DIR/
 rm -f $(printf "terraform_%s_linux_amd64.zip" $TF_VERSION)
 
-$BASE_DIR/tools/terraform -v
+$BASE_DIR/terraform -v
